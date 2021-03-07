15f2b09b6b5e43ff6e93d2195f187b1f1299b446
Arthur_air14 <954720981@qq.com>
2021-03-07 16:58:59 +0800

    更新测试，向上取整

--- Search/11.binary_search.cpp
+++ Search/11.binary_search.cpp
@@ -2,6 +2,10 @@
 #include<stdlib.h>
 #include<time.h>
 
+#define P(func) {\
+    printf("%s = %d\n", #func, func);\
+}
+
 int binary_search(int *arr, int n, int x){
     int head = 0, tail = n - 1, mid;
     while(head <= tail) {
@@ -16,8 +20,8 @@ int binary_search(int *arr, int n, int x){
 //11111111110000000000查找最后一个1
 int binary_search1(int *arr, int n, int x){
     int head = -1, tail = n - 1, mid;//head,虚拟位
-    while (head < tail) {
-        mid = (head + tail) >> 2;
+    while (head < tail + 1) {
+        mid = (head + tail) >> 1;
         if (arr[mid] == x) head = mid;
         else tail = mid - 1;
     }
@@ -25,11 +29,18 @@ int binary_search1(int *arr, int n, int x){
 
 }
 
-//0000000000001111111111111111111
+//0000000000001111111111111111111    查找第一个1
 int binary_search2(int *arr, int n, int x){
-    int head = 1, tail = n, mid;//
+    int head = 1, tail = n, mid;//tail为虚拟位
+    while (head < tail) {
+        mid = (head + tail + 1) >> 1;
+        if (arr[mid] == x) tail = mid;
+        else head = mid + 1;
+    }
+    return head == n ? -1 : head;
 }
 
+/*
 void randint(int *arr, int n) {
     while (n--) arr[n] = rand() % 100;
     return ;
@@ -44,12 +55,14 @@ void output(int *arr, int n) {
     printf("]\n");
     return ;
 }
+*/
 
 int main(){
-    #define MAX_N 10
-    srand(time(0));
-    int arr[MAX_N];
-    randint(arr, MAX_N);
-    
+    int arr1[10] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+    int arr2[10] = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+    int arr3[10] = {0, 0, 0, 0, 1, 1, 1, 1, 1, 1};
+    P(binary_search(arr1, 10, 7));
+    P(binary_search1(arr2, 10, 7));
+    P(binary_search2(arr3, 10, 7));
     return 0;
 }
